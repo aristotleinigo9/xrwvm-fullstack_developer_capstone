e900b87f3ea54db53260683d6574d46fdbd2e6fb
aristotleinigo9 <99675563+aristotleinigo9@users.noreply.github.com>
2024-06-15 19:49:46 +0300

    Update models.py

--- server/djangoapp/models.py
+++ server/djangoapp/models.py
@@ -2,18 +2,17 @@
 from django.db import models
 # from django.utils.timezone import now
 from django.core.validators import MaxValueValidator, MinValueValidator
-
 # Create your models here.
 # <HINT> Create a Car Make model `class CarMake(models.Model)`:
 # - Name
 # - Description
 # - Any other fields you would like to include in car make model
 # - __str__ method to print a car make object
+
 class CarMake(models.Model):
     name = models.CharField(max_length=100)
     description = models.TextField()
     # Other fields as needed
-
     def __str__(self):
         return self.name  # Return the name as the string representation     
 # <HINT> Create a Car Model model `class CarModel(models.Model):`:
