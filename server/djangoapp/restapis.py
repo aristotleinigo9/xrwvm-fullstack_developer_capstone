6e53ef32686b1a311cf5a0f2802a42a318fb21cb
aristotleinigo9 <99675563+aristotleinigo9@users.noreply.github.com>
2024-06-15 22:49:24 +0300

    Update restapis.py

--- server/djangoapp/restapis.py
+++ server/djangoapp/restapis.py
@@ -23,7 +23,7 @@ def get_request(endpoint, **kwargs):
             params += key + "=" + value + "&"
 
     request_url = backend_url + endpoint + "?" + params
-  
+
     print("GET from {} ".format(request_url))
     try:
         # Call get method of requests library with URL and parameters
