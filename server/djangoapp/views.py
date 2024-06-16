2bb51f67ad5ea994f92eda62c587b7704c1f1981
aristotleinigo9 <99675563+aristotleinigo9@users.noreply.github.com>
2024-06-16 06:22:02 +0300

    Update views.py

--- server/djangoapp/views.py
+++ server/djangoapp/views.py
@@ -19,27 +19,30 @@ logger = logging.getLogger(__name__)
 
 # Create a `login_user` view to handle sign in request
 
+
 @csrf_exempt
 def login_user(request):
     data = json.loads(request.body)
     username = data['userName']
     password = data['password']
-    
+
     user = authenticate(username=username, password=password)
     data = {"userName": username}
-    
+
     if user is not None:
         login(request, user)
         data = {"userName": username, "status": "Authenticated"}
-    
+
     return JsonResponse(data)
 
 # Create a `logout_request` view to handle sign out request
+
 def logout_request(request):
     data = {"userName": ""}
     return JsonResponse(data)
 
 # Create a `registration` view to handle sign up request
+
 @csrf_exempt
 def registration(request):
     if request.method == 'POST':
@@ -49,31 +52,35 @@ def registration(request):
         first_name = data['firstName']
         last_name = data['lastName']
         email = data['email']
-        
+
         try:
             User.objects.get(username=username)
             data = {"userName": username, "error": "Already Registered"}
         except User.DoesNotExist:
             logger.debug(f"{username} is a new user")
-            user = User.objects.create_user(username=username, first_name=first_name, last_name=last_name, password=password, email=email)
+            user = User.objects.create_user(username=username,
+                                            first_name=first_name,
+                                            last_name=last_name,
+                                            password=password,
+                                            email=email)
             login(request, user)
             data = {"userName": username, "status": "Authenticated"}
-        
+
         return JsonResponse(data)
 
 # Create a `get_cars` view to retrieve car models and makes
+
 @csrf_exempt
 def get_cars(request):
     count = CarMake.objects.count()
     if count == 0:
         initiate()
-    
+
     car_models = CarModel.objects.select_related('car_make')
     cars = [{"CarModel": car_model.name, "CarMake": car_model.car_make.name} for car_model in car_models]
     
     return JsonResponse({"CarModels": cars})
 
-# Update the `get_dealerships` view to render the index page with a list of dealerships
 
 def get_dealerships(request, state="All"):
     if state == "All":
@@ -86,11 +93,12 @@ def get_dealerships(request, state="All"):
 
 # Create a `get_dealer_reviews` view to render the reviews of a dealer
 
+
 def get_dealer_reviews(request, dealer_id):
     if dealer_id:
         endpoint = f"/fetchReviews/dealer/{dealer_id}"
         reviews = get_request(endpoint)
-        
+
         for review_detail in reviews:
             response = analyze_review_sentiments(review_detail['review'])
             review_detail['sentiment'] = response['sentiment']
@@ -100,6 +108,7 @@ def get_dealer_reviews(request, dealer_id):
         return JsonResponse({"status": 400, "message": "Bad Request"})
 
 # Create a `get_dealer_details` view to render the dealer details
+
 
 def get_dealer_details(request, dealer_id):
     if dealer_id:
@@ -111,11 +120,12 @@ def get_dealer_details(request, dealer_id):
 
 # Create an `add_review` view to submit a review
 
+
 def add_review(request):
     if not request.user.is_anonymous:
         data = json.loads(request.body)
         try:
-            response = post_review(data)
+            # response = post_review(data)
             return JsonResponse({"status": 200})
         except Exception as e:
             return JsonResponse({"status": 401,
